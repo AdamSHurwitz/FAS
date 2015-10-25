fc9ae58b6779566b73177c87bebeee414e61c187
dmikkili <dmikkili0@gmail.com>
2015-10-24 17:33:42 -0700

    Integrated tabs with drawer view.

--- app/src/main/java/com/example/adamhurwitz/fas/MainActivity.java
+++ app/src/main/java/com/example/adamhurwitz/fas/MainActivity.java
@@ -1,18 +1,60 @@
 package com.example.adamhurwitz.fas;
 
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
+    private DrawerLayout mDrawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+//        if (navigationView != null) {
+////            setupDrawerContent(navigationView);
+//        }
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.view_pager);
+        if (viewPager != null) {
+            setupViewPager(viewPager);
+            TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+            tabLayout.setupWithViewPager(viewPager);
+        }
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        if (navigationView != null) {
+            setupDrawerContent(navigationView);
+        }
+
     }
 
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        menuItem.setChecked(true);
+                        mDrawerLayout.closeDrawers();
+                        return true;
+                    }
+                });
+    }
+    private void setupViewPager(ViewPager viewPager) {
+        TabsAdapter adapter = new TabsAdapter(getSupportFragmentManager());
+        adapter.addFragment(new TabFragment(), "Category 1");
+        adapter.addFragment(new TabFragment(), "Category 2");
+        adapter.addFragment(new TabFragment(), "Category 3");
+        viewPager.setAdapter(adapter);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- app/src/main/java/com/example/adamhurwitz/fas/NavTabsFragment.java
+++ app/src/main/java/com/example/adamhurwitz/fas/NavTabsFragment.java
@@ -1,110 +1,110 @@
-package com.example.adamhurwitz.fas;
-
-import android.os.Bundle;
-import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-import java.util.Locale;
-
-/**
- * Created by adamhurwitz on 10/19/15.
- */
-public class NavTabsFragment extends Fragment {
-    public NavTabsFragment() {
-    }
-
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
-    SectionsPagerAdapter mSectionsPagerAdapter;
-
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
-    ViewPager mViewPager;
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_main, container, false);
-
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getActivity().getSupportFragmentManager());
-
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) view.findViewById(R.id.pager);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
-
-        //Add a tab bar navigation
-        TabLayout tabLayout = (TabLayout) view.findViewById(R.id.tabbar);
-        tabLayout.setupWithViewPager(mViewPager);
-
-        //------------------------------------------------------------------------------------------
-        // Attach dummyData
-
-
-        //------------------------------------------------------------------------------------------
-
-        return view;
-    }
-
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            switch (position) {
-                case 0:
-                    return PlaceholderFragment.newInstance(position + 1);
-                case 1:
-                    return Tab2.newInstance(position + 1);
-                case 2:
-                    return Tab3.newInstance(position + 1);
-            }
-            return null;
-        }
-
-
-        @Override
-        public int getCount() {
-            // Show 3 total pages.
-            return 3;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            Locale l = Locale.getDefault();
-            switch (position) {
-                case 0:
-                    return getString(R.string.title_section1).toUpperCase(l);
-                case 1:
-                    return getString(R.string.title_section2).toUpperCase(l);
-                case 2:
-                    return getString(R.string.title_section3).toUpperCase(l);
-            }
-            return null;
-        }
-    }
-
-}
+//package com.example.adamhurwitz.fas;
+//
+//import android.os.Bundle;
+//import android.support.design.widget.TabLayout;
+//import android.support.v4.app.Fragment;
+//import android.support.v4.app.FragmentManager;
+//import android.support.v4.app.FragmentPagerAdapter;
+//import android.support.v4.view.ViewPager;
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//
+//import java.util.Locale;
+//
+///**
+// * Created by adamhurwitz on 10/19/15.
+// */
+//public class NavTabsFragment extends Fragment {
+//    public NavTabsFragment() {
+//    }
+//
+//    /**
+//     * The {@link android.support.v4.view.PagerAdapter} that will provide
+//     * fragments for each of the sections. We use a
+//     * {@link FragmentPagerAdapter} derivative, which will keep every
+//     * loaded fragment in memory. If this becomes too memory intensive, it
+//     * may be best to switch to a
+//     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+//     */
+//    SectionsPagerAdapter mSectionsPagerAdapter;
+//
+//    /**
+//     * The {@link ViewPager} that will host the section contents.
+//     */
+//    ViewPager mViewPager;
+//
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                             Bundle savedInstanceState) {
+//        View view = inflater.inflate(R.layout.fragment_main, container, false);
+//
+//        // Create the adapter that will return a fragment for each of the three
+//        // primary sections of the activity.
+//        mSectionsPagerAdapter = new SectionsPagerAdapter(getActivity().getSupportFragmentManager());
+//
+//        // Set up the ViewPager with the sections adapter.
+//        mViewPager = (ViewPager) view.findViewById(R.id.pager);
+//        mViewPager.setAdapter(mSectionsPagerAdapter);
+//
+//        //Add a tab bar navigation
+//        TabLayout tabLayout = (TabLayout) view.findViewById(R.id.tabbar);
+//        tabLayout.setupWithViewPager(mViewPager);
+//
+//        //------------------------------------------------------------------------------------------
+//        // Attach dummyData
+//
+//
+//        //------------------------------------------------------------------------------------------
+//
+//        return view;
+//    }
+//
+//    /**
+//     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+//     * one of the sections/tabs/pages.
+//     */
+//    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+//
+//        public SectionsPagerAdapter(FragmentManager fm) {
+//            super(fm);
+//        }
+//
+//        @Override
+//        public Fragment getItem(int position) {
+//            // getItem is called to instantiate the fragment for the given page.
+//            // Return a PlaceholderFragment (defined as a static inner class below).
+//            switch (position) {
+//                case 0:
+//                    return PlaceholderFragment.newInstance(position + 1);
+//                case 1:
+//                    return Tab2.newInstance(position + 1);
+//                case 2:
+//                    return Tab3.newInstance(position + 1);
+//            }
+//            return null;
+//        }
+//
+//
+//        @Override
+//        public int getCount() {
+//            // Show 3 total pages.
+//            return 3;
+//        }
+//
+//        @Override
+//        public CharSequence getPageTitle(int position) {
+//            Locale l = Locale.getDefault();
+//            switch (position) {
+//                case 0:
+//                    return getString(R.string.title_section1).toUpperCase(l);
+//                case 1:
+//                    return getString(R.string.title_section2).toUpperCase(l);
+//                case 2:
+//                    return getString(R.string.title_section3).toUpperCase(l);
+//            }
+//            return null;
+//        }
+//    }
+//
+//}
