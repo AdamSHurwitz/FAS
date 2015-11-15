894e96fb1ac5d39fe0da234d9a86342ac69f8c3b
adamhurwitz <adamscotthurwitz@gmail.com>
2015-11-14 17:05:40 -0800

    DrawerView: Added Search, Favorites, and Cart Menu Items, Icons, and On Click Listeners launching Toasts

--- app/src/main/java/com/example/adamhurwitz/fas/MainActivity.java
+++ app/src/main/java/com/example/adamhurwitz/fas/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.adamhurwitz.fas;
 
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    // DrawerView: Initialize DrawerLayout - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    private DrawerLayout mDrawerLayout;
+    // ---------------------------------------------------------------------------------------------
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +28,81 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        // ----------------------------------------------------------------------------------------
+
+        // Navigation View
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+
+        // Click Listeners for DrawerView Menu Items
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+
+                    // This method will trigger on item Click of navigation menu
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+
+                        //Checking if the item is in checked state or not, if not make it in checked state
+                        if (menuItem.isChecked()) menuItem.setChecked(false);
+                        else menuItem.setChecked(true);
+
+                        //Closing drawer on item click
+                        mDrawerLayout.closeDrawers();
+
+                        //Check to see which item was being clicked and perform appropriate action
+                        switch (menuItem.getItemId()) {
+
+                            // For rest of the options we just show a toast on click
+
+                            case R.id.search_menu_itm:
+                                Toast.makeText(getApplicationContext(), "OPEN SEARCH BOX IN " +
+                                        "ACTION BAR", Toast.LENGTH_SHORT).show();
+                                return true;
+
+                            case R.id.favorites_menu_itm:
+                                Toast.makeText(getApplicationContext(), "LAUNCH FAVORITES ACTIVITY",
+                                        Toast.LENGTH_SHORT).show();
+
+                            case R.id.cart_menu_itm:
+                                Toast.makeText(getApplicationContext(), "LAUNCH CART ACTIVITY",
+                                        Toast.LENGTH_SHORT).show();
+                                return true;
+
+                            default:
+                                Toast.makeText(getApplicationContext(), "Somethings Wrong",
+                                        Toast.LENGTH_SHORT).show();
+                                return true;
+
+                        }
+                    }
+                });
+
+        // Initialize Drawer Layout and ActionBarToggle
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+                toolbar, R.string.openDrawer, R.string.closeDrawer) {
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                // Code here will be triggered once the drawer closes as we dont want anything to
+                // happen so we leave this blank
+                super.onDrawerClosed(drawerView);
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                // Code here will be triggered once the drawer open as we dont want anything to
+                // happen so we leave this blank
+
+                super.onDrawerOpened(drawerView);
+            }
+        };
+
+        //Setting the actionbarToggle to drawer layout
+        mDrawerLayout.setDrawerListener(actionBarDrawerToggle);
+
+        //calling sync state is necessay or else your hamburger icon wont show up
+        actionBarDrawerToggle.syncState();
+
         // ----------------------------------------------------------------------------------------
 
         // NavTabs: This loads viewPager for multiple tab views
