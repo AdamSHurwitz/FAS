6b368cbe2a91e69d30ad365cf6d29884055cc0df
adamhurwitz <adamscotthurwitz@gmail.com>
2016-03-04 19:15:17 -0800

    feat: Building FirebaseRecyclerView

--- app/src/main/java/com/example/adamhurwitz/fas/Adapter.java
+++ app/src/main/java/com/example/adamhurwitz/fas/Adapter.java
@@ -1,3 +1,4 @@
+/*
 package com.example.adamhurwitz.fas;
 
 import android.app.Activity;
@@ -10,38 +11,49 @@ import com.firebase.client.Query;
 import com.firebase.ui.FirebaseListAdapter;
 import com.squareup.picasso.Picasso;
 
+*/
 /**
  * Populates the list_view_active_lists inside ItemsFragment
- */
-public abstract class Adapter extends FirebaseListAdapter<Item> {
+ *//*
+
+public class Adapter extends FirebaseListAdapter<Item> {
     Context mContext;
 
-    /**
+    */
+/**
      * Public constructor that initializes private instance variables when adapter is created
-     */
+     *//*
+
     public Adapter(Activity activity, Class<Item> modelClass, int modelLayout,
                    Query ref) {
         super(activity, modelClass, modelLayout, ref);
         this.mActivity = activity;
     }
 
-    /**
+    */
+/**
      * Protected method that populates the view attached to the adapter (list_view_active_lists)
      * with items inflated from single_active_list.xml
      * populateView also handles data changes and updates the listView accordingly
-     */
+     *//*
+
     @Override
     protected void populateView(View view, Item list) {
 
-        /**
+        */
+/**
          * Grab the needed views and set data
-         */
+         *//*
+
         ImageView imageView = (ImageView) view.findViewById(R.id.adapter_item_imageview);
 
-       /* Set the list name and owner */
+       */
+/* Set the list name and owner *//*
+
         Picasso.with(mContext).load(Item.getImageUrl()).noFade()
                 .into(imageView);
         // textViewCreatedByUser.setText(list.getOwner());
     }
 }
 
+*/
--- app/src/main/java/com/example/adamhurwitz/fas/AdapterFragment.java
+++ app/src/main/java/com/example/adamhurwitz/fas/AdapterFragment.java
@@ -1,25 +1,37 @@
 package com.example.adamhurwitz.fas;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ListView;
-import android.widget.Toast;
+import android.widget.ImageView;
 
-import com.example.adamhurwitz.fas.utils.Constants;;
-import com.example.adamhurwitz.fas.model.Item;
+import com.example.adamhurwitz.fas.utils.Constants;
 import com.firebase.client.Firebase;
+import com.firebase.client.Query;
+import com.firebase.ui.FirebaseRecyclerAdapter;
+import com.squareup.picasso.Picasso;
+
+;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 // public class AdapterFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
 public class AdapterFragment extends Fragment {
-    private ListView mListView;
-    private Adapter mAdapter;
+    private final String LOG_TAG = AdapterFragment.class.getSimpleName();
+    //private ListView mListView;
+    private Firebase mRef;
+    private String string;
+    RecyclerView mListView;
+    private RecyclerView mImages;
+    private FirebaseRecyclerAdapter<Item, ItemHolder> mRecycleViewAdapter;
+    private Query mImageRef;
+    Context mContext;
 
     /**
      * Empty constructor for the PopularFragment class.
@@ -27,8 +39,6 @@ public class AdapterFragment extends Fragment {
     public AdapterFragment() {
         /* Required empty public constructor */
     }
-
-    private final String LOG_TAG = AdapterFragment.class.getSimpleName();
 
     /**
      * Initialize instance variables with data from bundle
@@ -46,35 +56,52 @@ public class AdapterFragment extends Fragment {
          * Initialize UI elements
          */
         View rootView = inflater.inflate(R.layout.adapter_view_layout, container, false);
-        initializeScreen(rootView);
+        //initializeScreen(rootView);
 
         /**
          * Create Firebase references
          */
-        Firebase activeListsRef = new Firebase(Constants.FIREBASE_URL_POPULAR_LIST);
+        Firebase mRef = new Firebase(Constants.FIREBASE_URL_POPULAR_LIST);
+
+        mImages = (RecyclerView) getActivity().findViewById(R.id.recyclerview_id);
+        LinearLayoutManager manager = new LinearLayoutManager(getActivity());
+        manager.setReverseLayout(false);
+        mImages.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mImages.setHasFixedSize(true);
+        mImages.setLayoutManager(manager);
+
+        mRecycleViewAdapter = new FirebaseRecyclerAdapter<Item, ItemHolder>(Item.class,
+                R.layout.adapter_item_layout, ItemHolder.class, mImageRef) {
+            @Override
+            public void populateViewHolder(ItemHolder imageView, Item image, int position) {
+                imageView.setImage(image.getImageUrl());
+            }
+        };
+
+        mImages.setAdapter(mRecycleViewAdapter);
 
         /**
          * Add ValueEventListeners to Firebase references
          * to control get data and control behavior and visibility of elements
          */
-        mAdapter = new Adapter(getActivity(), Item.class,
-                R.layout.adapter_item_layout, activeListsRef);
+        /*mAdapter = new Adapter(getActivity(), Item.class,
+                R.layout.adapter_item_layout, activeListsRef);*/
 
         /**
          * Set the adapter to the mListView
          */
-        mListView.setAdapter(mAdapter);
+        // mListView.setAdapter(mAdapter);
 
 
         /**
          * Set interactive bits, such as click events and adapters
          */
-        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        /*mImages.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Toast.makeText(getContext(), "Click Listener", Toast.LENGTH_SHORT).show();
             }
-        });
+        });*/
 
         return rootView;
     }
@@ -82,8 +109,45 @@ public class AdapterFragment extends Fragment {
     /**
      * Link list view from XML
      */
-    private void initializeScreen(View rootView) {
+  /*  private void initializeScreen(View rootView) {
         mListView = (ListView) rootView.findViewById(R.id.listview_id);
+    }*/
+
+    @Override
+    public Firebase getFirebaseRef() {
+        return mRef;
+    }
+
+    public static class Item {
+        String imageUrl;
+
+        public Item() {
+
+        }
+
+        public Item(String image_url) {
+            this.imageUrl = image_url;
+        }
+
+        public String getImageUrl() {
+            return imageUrl;
+        }
+    }
+
+    public static class ItemHolder extends RecyclerView.ViewHolder {
+        View mView;
+        Context mContext;
+
+        public ItemHolder(View itemView) {
+            super(itemView);
+            mView = itemView;
+        }
+
+        public void setImage(String imageUrl) {
+            ImageView imageView = (ImageView) mView.findViewById(R.id.adapter_item_imageview);
+            Picasso.with(mContext).load(imageUrl).noFade()
+                    .into(imageView);
+        }
     }
 }
 
